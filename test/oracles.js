118367f6ffe3609f2b64a2bef04cc6b9348f8988
heitho <thomasheil@Thomass-MacBook-Pro.local>
2020-06-16 20:44:10 +0200

    still debugging

--- test/oracles.js
+++ test/oracles.js
@@ -39,6 +39,7 @@ contract('Oracles', async (accounts) => {
     let flight = 'ND1309'; // Course number
     let timestamp = Math.floor(Date.now() / 1000);
 
+    var success = 0;
     // Submit a request for oracles to get status information for a flight
     await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
     // ACT
@@ -56,52 +57,21 @@ contract('Oracles', async (accounts) => {
         try {
           // Submit a response...it will only be accepted if there is an Index match
           await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
+          success += 1;
           console.log('Success', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
         catch(e) {
           // Enable this when debugging
-          console.log(e)
+          //console.log(e)
           console.log('Error', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
 
       }
     }
+    assert.equal(success != 0, true, "no successful oracle response");
   });
 
-  it('can request flight status', async () => {
-    
-    // ARRANGE
-    let flight = 'ND1309'; // Course number
-    let timestamp = Math.floor(Date.now() / 1000);
-
-    // Submit a request for oracles to get status information for a flight
-    await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
-    // ACT
-
-    // Since the Index assigned to each test account is opaque by design
-    // loop through all the accounts and for each account, all its Indexes (indices?)
-    // and submit a response. The contract will reject a submission if it was
-    // not requested so while sub-optimal, it's a good test of that feature
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {
-
-      // Get oracle information
-      let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({ from: accounts[a]});
-      for(let idx=0;idx<3;idx++) {
-
-        try {
-          // Submit a response...it will only be accepted if there is an Index match
-          await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
-          console.log('Success', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
-        }
-        catch(e) {
-          // Enable this when debugging
-          console.log(e)
-          console.log('Error', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
-        }
-
-      }
-    }
-  });
+  
 
   it('can request future flight status', async () => {
     
@@ -112,7 +82,7 @@ contract('Oracles', async (accounts) => {
     // Submit a request for oracles to get status information for a flight
     await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, config.flightTime);
     // ACT
-
+    var success = 0;
     // Since the Index assigned to each test account is opaque by design
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
@@ -126,15 +96,18 @@ contract('Oracles', async (accounts) => {
         try {
           // Submit a response...it will only be accepted if there is an Index match
           await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, config.flightTime, STATUS_CODE_LATE_AIRLINE, { from: accounts[a] });
+          success += 1;
           console.log('Success', idx, oracleIndexes[idx].toNumber(), flight, config.flightTime);
         }
         catch(e) {
           // Enable this when debugging
-           console.log('Error', idx, oracleIndexes[idx].toNumber(), flight, config.flightTime);
+          console.log(e)
+          console.log('Error', idx, oracleIndexes[idx].toNumber(), flight, config.flightTime);
         }
 
       }
     }
+    assert.equal(success != 0, true, "no successful oracle response");
   });
 
  
